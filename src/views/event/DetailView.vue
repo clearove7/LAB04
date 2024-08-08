150baf1edc0d60ca3af10feaafec0f5bf228193a
Zhenyu_Pan <zhenyu_pan@cmu.ac.th>
2024-08-09 00:39:25 +0700

    DO 1

---
+++ src/views/event/DetailView.vue
@@ -0,0 +1,45 @@
+<!-- src/views/event/DetailView.vue -->
+<script setup lang="ts">
+import { toRefs, defineProps, onMounted } from 'vue'
+import { type Event } from '@/types'
+import { useMessageStore } from '@/stores/message'
+import { storeToRefs } from 'pinia'
+
+const props = defineProps<{
+  event: Event
+  id: String
+}>()
+const { event } = toRefs(props)
+const store = useMessageStore()
+const { message } = storeToRefs(store)
+
+onMounted(() => {
+  if (message.value) {
+    setTimeout(() => {
+      store.resetMessage()
+    }, 3000)
+  }
+})
+</script>
+
+<template>
+  <div v-if="message" id="flashMessage">
+    <h4>{{ message }}</h4>
+  </div>
+  <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+  <p>{{ event.description }}</p>
+</template>
+
+<style scoped>
+@keyframes yellofade {
+  from {
+    background-color: yellow;
+  }
+  to {
+    background-color: transparent;
+  }
+}
+#flashMessage {
+  animation: yellofade 3s ease-in-out;
+}
+</style>
---
+++ src/views/event/EditView.vue
@@ -0,0 +1,29 @@
+<!-- src/views/event/EditView.vue -->
+<script setup lang="ts">
+import { toRef } from 'vue'
+import { type Event } from '@/types'
+import { useRouter } from 'vue-router'
+import { useMessageStore } from '@/stores/message'
+
+const props = defineProps<{
+    event: Event
+    id: String
+}>()
+// eslint-lisable-next-line @typescript-eslint/no-unused-vars
+const { event } = toRef(props)
+const router = useRouter()
+const store = useMessageStore()
+
+const updateEvent = () => {
+    store.updateMessage('Event data has been updated')
+    setTimeout(() => {
+        store.resetMessage()
+    }, 3000)
+    router.push({ name: 'event-detail-view', params: { id: props.event.id } })
+}
+</script>
+
+<template>
+    <p>Edit event here</p>
+    <button @click="updateEvent">Save Changes</button>
+</template>
